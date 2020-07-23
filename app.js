9e856b5d9525615a69e0602e51ed2dcce51b803c
GarciaFrida <64746413+GarciaFrida@users.noreply.github.com>
2020-07-23 15:32:10 -0400

    working on adding items to completed list

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
 const pgp = require('pg-promise')();
-const connect = require('./config')
-const db = pgp(connect);
-const port = 5005
+const connect = require('./config');
 const path = require('path');
+const db = pgp(connect);
 
-app.use(express.urlencoded({extended:true}))
+const port = 5005;
+
+// app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get("/", (req,res)=>res.sendFile(__dirname + path.join("/site/index.html")))
 
 
-app.get("/task", async (req,res)=>{
+app.get("/tasks", async (req,res)=>{
     const data = await db.any('SELECT * FROM tasks')
     res.json(data)
 })
 
 app.get("/newtask", (req,res)=>{
-    res.sendFile(__dirname + path.join("/html/newtask.html"))
+    res.sendFile(__dirname + path.join("/site/newtask.html"))
 })
 
 
@@ -29,12 +33,12 @@ app.patch('/edit-task/:id/complete_task', async (req,res)=>{
     let result = await db.one(`
         UPDATE tasks 
         SET is_completed = 'true' 
-        WHERE id ='${req.params.id} RETURNING *`
+        WHERE id ='${req.params.id}' RETURNING *`
     );
     res.send(result);
 
 })
-app.patch('/edit-task/:id/un_complete_task',)
+app.patch('/edit-task/:id/title',)
 
 app.listen(port, ()=>{
     console.log(`http://localhost${port}`)
